10f4bb02bb02a9b8a0930bbf21917800009218ec
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-28 11:20:00 -0600

    Fixed migration

--- migrations/20160328100417_initial_author_table.js
+++ migrations/20160328100417_initial_author_table.js
@@ -4,18 +4,10 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.integer('book_id');
     table.foreign('book_id').references('id').inTable('books');
-    table.string('author_1_first_name');
-    table.string('author_1_last_name');
-    table.text('author_1_biography');
-    table.text('author_1_portrait_url');
-    table.string('author_2_first_name');
-    table.string('author_2_last_name');
-    table.text('author_2_biography');
-    table.text('author_2_portrait_url');
-    table.string('author_3_first_name');
-    table.string('author_3_last_name');
-    table.text('author_3_biography');
-    table.text('author_3_portrait_url');
+    table.string('first_name');
+    table.string('last_name');
+    table.text('biography');
+    table.text('portrait_url');
   });
 };
 
