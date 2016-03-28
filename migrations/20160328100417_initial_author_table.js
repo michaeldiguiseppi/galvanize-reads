9ab5b23fe7f97e3766b5ecf032f926bbb1692809
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-28 11:53:09 -0600

    Added seed file for authors and added join table migration.

--- migrations/20160328100417_initial_author_table.js
+++ migrations/20160328100417_initial_author_table.js
@@ -2,8 +2,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('authors', function(table) {
     table.increments();
-    table.integer('book_id');
-    table.foreign('book_id').references('id').inTable('books');
     table.string('first_name');
     table.string('last_name');
     table.text('biography');
