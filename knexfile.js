0d835fcb64fdde751951841ff5d1985016d69785
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-29 14:55:14 -0600

    Fixed knexfile and removed commented code.

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.PROD_DB + '?ssl=true'
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -3,14 +3,3 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
-
-// $('#delete').on('click', function(event) {
-//   event.preventDefault();
-//   delConfirm();
-// });
-//
-// function delConfirm() {
-//   bootbox.confirm('Are you sure you want to delete this?', function(result) {
-//
-//   });
-// }
